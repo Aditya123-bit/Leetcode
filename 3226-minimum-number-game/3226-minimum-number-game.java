2e4738c6e0adfb203cdd3906b63ca128b430336a
Adi <adityakumaryadav2598@gmail.com>
2025-09-25 15:36:45 +0530

    Time: 2 ms (97.64%), Space: 44.6 MB (96.59%) - LeetHub

--- 3226-minimum-number-game/3226-minimum-number-game.java
+++ 3226-minimum-number-game/3226-minimum-number-game.java
@@ -2,13 +2,10 @@ class Solution {
     public int[] numberGame(int[] nums) {
         Arrays.sort(nums);
         for(int i = 0; i<nums.length; i = i+2){
-            Swap(nums , i , i+1);
+            int temp = nums[i];
+            nums[i] = nums[i+1];
+            nums[i+1] = temp;
         }
         return nums;
-    }
-    public static void Swap(int[] nums,int a,int b){
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
     }
 }
