16e7a3082a579417d78efb679ac0d0f510a368b4
Adi <adityakumaryadav2598@gmail.com>
2025-11-13 00:48:11 +0530

    Time: 47 ms (14.68%), Space: 79.1 MB (6.85%) - LeetHub

--- 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.java
+++ 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.java
@@ -1,30 +1,30 @@
 class Solution {
-    public int maxDistance(int[] stalls, int k) {
-        Arrays.sort(stalls);
-        int n = stalls.length;
+    public int maxDistance(int[] position, int m) {
+        Arrays.sort(position);
+        int n = position.length;
         int ans = -1;
         
         int low = 0;
-        int high = stalls[n-1] - stalls[0];
+        int high = position[n-1] - position[0];
         while(low <= high){
             int mid = low + (high - low)/2;
-            if(canWePlaceCow(stalls,mid,k) == true){
+            if(canWePlaceball(position,mid,m) == true){
                 ans = mid;
                 low = mid + 1; 
             }else high = mid - 1;
         }
         return ans;
     }
-    public static boolean canWePlaceCow(int[] stalls,int dist,int cow){
-        int countcow = 1;
-        int lastcow = stalls[0];
+    public static boolean canWePlaceball(int[] position,int dist,int ball){
+        int countball = 1;
+        int lastball = position[0];
         
-        for(int i = 0; i < stalls.length; i++){
-            if(stalls[i] - lastcow >= dist){
-                countcow++;
-                lastcow = stalls[i];
+        for(int i = 0; i < position.length; i++){
+            if(position[i] - lastball >= dist){
+                countball++;
+                lastball = position[i];
             }
-            if(countcow >= cow) return true;
+            if(countball >= ball) return true;
         }
         return false;
     }
