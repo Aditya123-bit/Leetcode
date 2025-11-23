f5fd1c2c6fcb2dd68925024a61019b426a790da1
Adi <adityakumaryadav2598@gmail.com>
2025-11-24 00:09:39 +0530

    Time: 214 ms (25.46%), Space: 12.6 MB (64.3%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        int count = 0;
-
         vector<bool> prime(n+1,true);
-
         prime[0] = prime[1] = false;
 
+        int count = 0;
         for(int i = 2; i < n; i++){
             if(prime[i]){
                 count++;
