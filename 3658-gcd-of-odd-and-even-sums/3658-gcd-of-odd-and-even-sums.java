f78d5e373275f61781ad8ea35b1e2a0c847b5a98
Adi <adityakumaryadav2598@gmail.com>
2025-11-27 23:32:13 +0530

    Time: 1 ms (60.53%), Space: 42.7 MB (11.18%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,23 +1,34 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int sumOdd = 0;
-        int sumEven = 0;
+    //     int sumOdd = 0;
+    //     int sumEven = 0;
 
-        for(int i = 1; i <= 2*n; i++){
-            if(i % 2 == 0){
-                sumEven += i;
-            }else sumOdd += i;
-        }
+    //     for(int i = 1; i <= 2*n; i++){
+    //         if(i % 2 == 0){
+    //             sumEven += i;
+    //         }else sumOdd += i;
+    //     }
 
+    //     return gcd(sumOdd, sumEven);
+    // }
+    // public static int gcd(int a, int b){
+    //     while(a > 0 && b >0){
+    //         if(a > b){
+    //             a %= b;
+    //         }else b %= a;
+    //     }
+    //     if(a == 0) return b;
+    //     return a;
+            int sumOdd = n * n;
+        int sumEven = n * (n + 1);
         return gcd(sumOdd, sumEven);
     }
-    public static int gcd(int a, int b){
-        while(a > 0 && b >0){
-            if(a > b){
-                a %= b;
-            }else b %= a;
+     private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
         }
-        if(a == 0) return b;
         return a;
     }
 }
