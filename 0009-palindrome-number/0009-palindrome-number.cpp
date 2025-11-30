70e59e6e57fca8ed9454f4a121f5aa5d038dabb5
Adi <adityakumaryadav2598@gmail.com>
2025-11-30 10:03:36 +0530

    Time: 1 ms (58.78%), Space: 8.8 MB (27.25%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -4,9 +4,9 @@ public:
         if (x < 0) return false;  
 
         int original = x;
-         long rev = 0; 
+        long rev = 0; 
 
-        while (x != 0) {
+        while (x > 0) {
             int ld = x % 10;
             rev = rev * 10 + ld;
             x /= 10;
