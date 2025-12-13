1f5d0fa13821d4a88d83cb343f17a50b5744bcdf
Adi <adityakumaryadav2598@gmail.com>
2025-12-13 22:08:18 +0530

    Time: 1 ms (99.9%), Space: 42.8 MB (11.92%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.java
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.java
@@ -3,21 +3,19 @@ class Solution {
         List<Integer> res = new ArrayList<>();
 
         for(int i = left; i <= right; i++){
-            if(check(i)){
-                res.add(i);
-            }
+            if(check(i)) res.add(i);
         }
+        
     return res;
     }
     private boolean check(int n){
         int temp = n;
-        
+
         while(temp > 0){
             int digit =  temp % 10;
 
-            if(digit == 0 || n % digit != 0){
-                return false;
-            }
+            if(digit == 0 || n % digit != 0) return false;
+
             temp = temp/10;
         }
     return true;
