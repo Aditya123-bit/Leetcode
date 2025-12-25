293556764822561cecb1597f2926eb4d3303d8c5
Adi <adityakumaryadav2598@gmail.com>
2025-12-26 03:19:09 +0530

    Time: 0 ms (100%), Space: 12.3 MB (49.82%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -3,12 +3,13 @@ public:
     string reverseWords(string s) {
         int start = 0;
 
-        for(int i = 0; i <= s.length(); i++){
-            if(s[i] == ' ' || i == s.length()){
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == ' '){
                 reverse(s.begin() + start, s.begin() + i);
                 start = i+1;
             }
         }
+        reverse(s.begin() + start, s.end());
         
         return s;
     }
